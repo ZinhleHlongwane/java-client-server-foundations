a8f96a37ee319913c52ea4b3769ac684f6ac9e51
Zinhle Hlongwane <zihlojhb025@student.wethinkcode.co.za>
2026-05-07 09:44:42 +0200

    Create server entry point to start MultiServer

--- src/main/java/za/co/practice/server/Server.java
+++ src/main/java/za/co/practice/server/Server.java
@@ -2,4 +2,11 @@ package za.co.practice.server;
 
 public class Server {
 
+    public static void main(String[] args) {
+        MultiServer server = new MultiServer();
+        server.start();
+    }
+
 }
+
+// Starts the program
