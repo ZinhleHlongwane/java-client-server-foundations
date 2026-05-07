0bfcecb97b48805914cb62dc10f625c80a6bbfa5
Zinhle Hlongwane <zihlojhb025@student.wethinkcode.co.za>
2026-05-07 10:36:14 +0200

    Create client entry point for starting client connection

--- src/main/java/za/co/practice/client/Client.java
+++ src/main/java/za/co/practice/client/Client.java
@@ -2,4 +2,13 @@ package za.co.practice.client;
 
 public class Client {
 
+    public static void main(String[] args) {
+
+        ClientConnection connection = new ClientConnection();
+
+        connection.connect();
+    }
+
 }
+
+// Starts the client program
