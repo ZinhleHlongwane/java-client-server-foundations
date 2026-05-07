b8157dc61e35dee0eba297eb909dda782673cd17
Zinhle Hlongwane <zihlojhb025@student.wethinkcode.co.za>
2026-05-07 10:32:47 +0200

    Build MultiServer with ServerSocket startup and single client acceptance

--- src/main/java/za/co/practice/server/MultiServer.java
+++ src/main/java/za/co/practice/server/MultiServer.java
@@ -1,5 +1,30 @@
 package za.co.practice.server;
 
+import java.io.IOException; //Jva forces us to handle networking errors
+import java.net.ServerSocket; // Creates the listening server
+import java.net.Socket; // Represents one connected client
+
 public class MultiServer {
+    public void start() {
+
+        try {
+            // Open server on port 5000
+            ServerSocket serverSocket = new ServerSocket(5000);
+
+            System.out.println("Server started on port 5000.");
+
+            // Wait for one client
+            Socket clientSocket = serverSocket.accept();
+
+            System.out.println("Client connected!");
+        } catch (IOException e) {
+            System.out.println("Server error occured.");
+        }
+    }
 
 }
+
+// Opens the port and waits for clients
+// The server must open a port
+// Wait for a client ClientConnection
+// Accept that client connection
